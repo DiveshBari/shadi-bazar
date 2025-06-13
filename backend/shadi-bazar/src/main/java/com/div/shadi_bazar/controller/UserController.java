6e9ba00f1766b0f3c583ad45813293c0d60cdd1e
DiveshBari <diveshbari1010@gmail.com>
2025-06-13 20:08:26 +0530

    Add user registration and profile update functionality with utility for decoding credentials

--- backend/shadi-bazar/src/main/java/com/div/shadi_bazar/controller/UserController.java
+++ backend/shadi-bazar/src/main/java/com/div/shadi_bazar/controller/UserController.java
@@ -2,8 +2,10 @@ package com.div.shadi_bazar.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.div.shadi_bazar.Utils.StringUtils;
 import com.div.shadi_bazar.dto.UserLoginDTO;
 import com.div.shadi_bazar.dto.UserRegistrationDTO;
+import com.div.shadi_bazar.dto.UserUpdateProfileDTO;
 import com.div.shadi_bazar.entity.User;
 import com.div.shadi_bazar.service.UserService;
 
@@ -11,12 +13,16 @@ import java.util.List;
 
 import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 
@@ -40,12 +46,8 @@ public class UserController {
      * The email and password should be sent in the Authorization header
      */
     public ResponseEntity<String> loginUser(@RequestHeader(value="Authorization") String token) {
-    	//token in the format "Basic base64(email:password)"
-        String base64Credentials = token.substring("Basic ".length()).trim();
-        // Decode the base64 encoded
-        byte[] decodedBytes = java.util.Base64.getDecoder().decode(base64Credentials);
-        String credentials = new String(decodedBytes);
-        
+        String credentials = StringUtils.getUserNameAndPasswordFromBase64(token);
+
         // Split the credentials into email and password
         String email = credentials.split(":")[0];
         String password = credentials.split(":")[1];
@@ -56,5 +58,53 @@ public class UserController {
     		return ResponseEntity.ok().body("Login successful");
     	else
     		return ResponseEntity.status(Response.SC_UNAUTHORIZED).build();
+    }
+
+    /*
+     * This endpoint allows a user to register by sending their details in the request body.
+     * The details should be in JSON format and match the UserRegistrationDTO structure.
+     */ 
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody UserRegistrationDTO dto) {
+        User user = userService.registerUser(dto);
+        if (user != null) {
+            return ResponseEntity.ok().body("User registered successfully");
+        } 
+        else {
+            return ResponseEntity.status(Response.SC_BAD_REQUEST).body("User registration failed");
+        }
+    }
+
+    /*
+     * Update user profile
+     *
+     * This endpoint allows a user to update their profile information.
+     * The updated details should be sent in the request body.
+     * The email parameter specifies which user's profile will be updated.
+     */
+    @PutMapping("/update-user")
+    public String updateUser(@RequestHeader(value = "Authorization") String token, @RequestBody UserUpdateProfileDTO dto) {        
+        String credentials = StringUtils.getUserNameAndPasswordFromBase64(token);
+        
+        // Split the credentials into email and password
+        String email = credentials.split(":")[0];
+        String password = credentials.split(":")[1];
+        
+        // Validate the user's credentials before updating the profile
+        User user = userService.loginUser(email, password);
+        if (user == null) {
+            return "Invalid credentials";
+        }
+
+        return userService.updateUserProfile(user, dto);
+    }
+    
+    /*
+     * This method handles any exceptions that occur during processing.
+     * It returns an HTTP 500 Internal Server Error response with the exception message as the body.
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleRuntimeException(Exception ex) {
+        return ResponseEntity.status(Response.SC_INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 }
--- backend/shadi-bazar/src/main/java/com/div/shadi_bazar/service/UserService.java
+++ backend/shadi-bazar/src/main/java/com/div/shadi_bazar/service/UserService.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.div.shadi_bazar.dto.UserLoginDTO;
+import com.div.shadi_bazar.dto.UserRegistrationDTO;
+import com.div.shadi_bazar.dto.UserUpdateProfileDTO;
 import com.div.shadi_bazar.entity.User;
 import com.div.shadi_bazar.repository.UserRepository;
 
@@ -37,6 +39,50 @@ public class UserService {
             return user; // Return the user if login is successful
         } else {
             return null; // Return null if login fails
+        }
+    }
+
+    /*
+     * Register a new user based on the provided registration data.
+     *
+     * @param dto The DTO containing user registration information.
+     * @return The registered user entity.
+     */
+    public User registerUser(UserRegistrationDTO dto) {
+        // Create a new user entity and save it
+        User user = new User();
+        user.setEmail(dto.getEmail());
+        user.setName(dto.getName());
+        user.setPassword(dto.getPassword());
+        user.setPhoneNumber(dto.getPhoneNumber());
+
+        user = userRepository.save(user);
+        return user;
+    }
+
+    /*
+     * Update an existing user's profile based on the provided update data.
+     *
+     * @param entity The DTO containing updated user information.
+     * @param email  The email address of the user to be updated.
+     * @return A message indicating whether the update was successful or not.
+     */
+    public String updateUserProfile(User existingUser, UserUpdateProfileDTO dto) {
+        // Check if the user exists by email
+        if (existingUser != null) {
+            // Update the user's details
+            User user = new User();
+            user.setId(existingUser.getId());
+            user.setEmail(existingUser.getEmail());
+            user.setName(dto.getName());
+            user.setPassword(dto.getPassword());
+            user.setPhoneNumber(dto.getPhoneNumber());
+            user = userRepository.save(user);
+
+            return "User updated successfully";
+        }
+        else{
+            throw new RuntimeException("User does not exist");
         }
     }
 }
